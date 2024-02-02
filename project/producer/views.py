2d5cfd5235e0568bbe498b80eac331a63e10c87a
saaddine <saadeddine.saad@elyadata.com>
2024-02-02 08:23:26 +0100

    add sending data to the consumer

--- project/producer/views.py
+++ project/producer/views.py
@@ -1,4 +1,7 @@
+import requests
+
 from rest_framework import viewsets, status
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -14,24 +17,34 @@ class MessageViewSet(viewsets.ModelViewSet):
     authentication_classes = [BasicAuthentication]
     permission_classes = [IsAuthenticated]
 
-    def list(self, request, *args, **kwargs):
-        messages = self.get_queryset()
-        serializer = self.get_serializer(messages, many=True)
-        return Response(serializer.data)
+    @action(detail=False, methods=['post'])
+    def create_and_send_to_consumer(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        message_instance = serializer.save()
+
+        data_to_send = {'text': message_instance.text, 'timestamp': message_instance.timestamp, 'id': message_instance.id}
+        response = requests.post('http://localhost:8001/consumer/api/process-message/', json=data_to_send)
+
+        # Process the response as needed
+        result_from_consumer = response.json().get('result', None)
+
+        return Response({'status': 'success'})
 
 
 class WebhookReceiverView(APIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request, *args, **kwargs):
-        result_data = request.data.get('result', None)
+        data_from_consumer = request.POST
 
-        if result_data is not None:
-            message_id = result_data.get('message_id', None)
+        if data_from_consumer is not None:
+            message_id = data_from_consumer.get('message_id')
             if message_id is not None:
                 try:
                     message = Message.objects.get(pk=message_id)
-                    message.result = result_data.get('result_value', None)
+                    message.result = data_from_consumer.get('result', None)
                     message.save()
 
                     return Response({'message': 'Result updated successfully'}, status=status.HTTP_200_OK)
